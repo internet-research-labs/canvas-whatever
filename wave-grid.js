b43053129815fd3210a607a5681b4be049da6891
Matt Owen <not.mattowen@gmail.com>
2016-08-26 08:34:05 -0400

    :cake:

--- wave-grid.js
+++ wave-grid.js
@@ -1,19 +1,37 @@
 function random(x, y) {
-  return Math.rand() * (y-x) + x
+  return Math.random() * (y-x) + x
 }
 
-function Piece(x, y, rad, color) {
+function Composed (a, b) {
+  this.a = a;
+  this.b = b;
+  this.setup();
+}
+
+Composed.prototype.setup = function () {
+  this.a.setup();
+  this.b.setup();
+};
+
+Composed.prototype.draw = function (ctx) {
+  this.a.draw(ctx);
+  this.b.draw(ctx);
+};
+
+function Piece(x, y, w, h, color) {
   this.x = x;
   this.y = y;
-  this.rad = rad;
+  this.w = w;
+  this.h = h;
   this.col = color;
   this.image = new Image();
   this.image.src= 'img/wave-3.jpg';
+  this.box = this.randomSlice(100, 100);
 }
 
 Piece.prototype.randomSlice = function (w, h) {
-  var sx = Math.floor(random(0, this.width - w));
-  var sy = Math.floor(random(0, this.width - h));
+  var sx = Math.floor(random(0, this.image.width - w));
+  var sy = Math.floor(random(0, this.image.height - h));
   return {
     'sx': sx,
     'sy': sy,
@@ -23,10 +41,65 @@ Piece.prototype.randomSlice = function (w, h) {
 }
 
 Piece.prototype.draw = function (ctx) {
+  var box = this.box;
+
   ctx.save();
-    ctx.drawImage(this.image, 0, 0);
+  ctx.drawImage(
+    this.image,
+    box.sx, box.sy,
+    box.sw, box.sh,
+    this.x, this.y,
+    this.w, this.h
+  );
+    ctx.globalCompositeOperation = 'lighten';
+
+    ctx.fillStyle = this.col;
+    ctx.rect(
+      this.x, this.y,
+      this.w, this.h
+    );
+    ctx.fill();
   ctx.restore();
 }
+
+function Grid (el, x, y, col) {
+  this.width = el.width;
+  this.height = el.height;
+  this.x = x;
+  this.y = y;
+  this.grid = null;
+  this.col = col;
+  this.setupGrid();
+}
+
+Grid.prototype.setupGrid = function () {
+  var self = this;
+  this.grid = new Array(this.x);
+  for (var j=0; j < this.x; j++) {
+    this.grid[j] = new Array(this.y);
+  }
+
+  var w = this.width/this.x;
+  var h = this.height/this.y;
+
+  for (var x=0; x < this.x; x++) {
+    for (var y=0; y < this.y; y++) {
+      self.grid[x][y] = new Piece(
+        x * w, y * h, 
+        w, h,
+        self.col
+      );
+    }
+  }
+};
+
+Grid.prototype.draw = function (ctx) {
+  this.grid.forEach(function (row, x) {
+    row.forEach(function (val, y) {
+      val.draw(ctx);
+    });
+  });
+};
 
 function WaveApp(id) {
   this.id = id;
@@ -37,8 +110,8 @@ WaveApp.prototype.setup = function () {
   this.width = this.el.width;
   this.height = this.el.height;
   this.ctx = this.el.getContext('2d');
-  this.grid1 = new Piece(100, 100, 100, 'red');
-  this.grid2 = new Piece(500, 500, 100, 'blue');
+  this.grid3 = new Grid(this.el, 6, 6, 'red');
+  this.grid4 = new Grid(this.el, 6, 6, 'blue');
 };
 
 WaveApp.prototype.update = function () {
@@ -49,8 +122,8 @@ WaveApp.prototype.draw = function () {
   this.clear();
   ctx.save();
     ctx.globalCompositeOperation = 'darken';
-    this.grid1.draw(ctx);
-    this.grid2.draw(ctx);
+    this.grid3.draw(ctx);
+    this.grid4.draw(ctx);
   ctx.restore();
 };
 
