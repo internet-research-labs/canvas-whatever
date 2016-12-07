d0ca6a30bc92858c36a6c90f564c1e33eddfea55
Matt Owen <not.mattowen@gmail.com>
2016-12-07 12:09:14 -0500

    :okay:

--- src/Compose.js
+++ src/Compose.js
@@ -1,36 +1,56 @@
-class Compose {
+/**
+ * Compose
+ * @param ... an object with an update(Object) and draw(context) method
+ * @return an object with an update(Object) and draw(context) method
+ */
+export default class Compose {
+  constructor() {
+    this.scene = arguments;
+  }
+
   /**
+   * Add a object to render to the scene
+   * @param obj Object an object to attach to the scene
+   * @return self
+   */
+  add(obj) {
+    this.scene.push(obj);
+    return this;
+  }
+
+  /**
+   * Return a list of objects that are being updated/drawn
+   * @return list[Object] a list of objects passed into the Compose constructor
+   */
+  getObjects() {
+    return this.scene;
+  }
+
+  /**
+   * Update each object withs params
+   * @param params Object an object containing a dict of values that may-or-may-not be
+   *    relevant for each object composed
+   * @return self
    *
    */
-  constructor (...objects) {
-    this.scenes = objects;
-  };
+  update(params) {
+    this.scene.forEach(function (val) {
+      val.update(params);
+    });
+    return this;
+  }
+
 
   /**
+   * Draw each object with context
+   * @param ctx Context a rendering context
+   * @return self
    *
    */
-  setup () {
-    this.scenes.forEach((scene) => {
-      scene.setup();
+  draw(ctx) {
+    this.scene.forEach(function (val) {
+      val.draw(ctx);
     });
-  };
-
-  /**
-   *
-   */
-  update (params) {
-    this.scenes.forEach((scene) => {
-      scene.update(params);
-    });
-  };
-
-  /**
-   */
-  draw (ctx) {
-    this.scenes.forEach((scene) => {
-      scene.draw(ctx);
-    });
-  };
-};
-
-export {Compose};
+    return this;
+  }
+}
