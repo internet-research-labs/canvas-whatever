723d1f32a8a12084f4bf584d33be0159c6bd7c44
Matt Owen <not.mattowen@gmail.com>
2016-12-21 19:31:11 -0500

    :joy:

--- src/LoadingCube.js
+++ src/LoadingCube.js
@@ -17,15 +17,35 @@ export default class LoadingCube {
   constructor(params) {
     this.el = params.el;
     this.apect = this.el.width/this.el.height;
-    this.cubes = [];
-    this.camera = new THREE.PerspectiveCamera( 50, 0.5 * this.aspect, 1, 10000 );
-    this.camera.position.x = 5;
-    this.camera.position.y = 5;
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({
+      wireframe: true,
+      color: 0x000000,
+    });
+    this.singleCube = new THREE.Mesh(geometry, material);
+
+    //this.singleCube.position.x = 0;
+    //this.singleCube.position.y = 0;
+    //this.singleCube.position.z = 0;
+
+
+    this.cubeList = [];
+
+    // this.camera.position.x = 5;
+    // this.camera.position.y = 5;
+    this.camera = new THREE.PerspectiveCamera(75, 1.0, 0.1, 1000 );
+
     this.camera.position.z = 5;
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     this.state = 'starting';
     this.scene = new THREE.Scene();
+    this.scene.add(this.singleCube);
+
+    this.singleCube.rotation.x += 0.1
+    this.singleCube.rotation.y += 0.8
+    this.singleCube.rotation.y -= 0.4
 
     // Setup renderer
     this.isRunning = true;
@@ -34,17 +54,18 @@ export default class LoadingCube {
     // ....
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    console.log(this.el.width, this.el.height);
     this.renderer.setSize(300, 300);
     this.renderer.setClearColor(0x000000, 1);
 
     // Attach canvas
     this.el.appendChild(this.renderer.domElement);
+
+    console.log("Finished creating");
   }
 
   start() {
     this.state = 'starting';
-    this.scene.background = new THREE.Color( 0xff0000 );
+    this.scene.background = new THREE.Color( 0xFFFFFF );
   }
 
   think() {
