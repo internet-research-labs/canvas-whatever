aa8bc4c2ce2db814f5ddde883278cddf666175a8
Matt Owen <matt.owen@moat.com>
2017-08-13 01:31:03 -0400

    :cherries:

--- src/TriangleSurface.js
+++ src/TriangleSurface.js
@@ -24,14 +24,13 @@ export default class TriangleSurface {
     for (let i=-10; i < 10; i++) {
       for (let j=-10; j < 10; j++) {
 
-        // console.log(i, j);
-
         let [a, b, c] = this.mesh.get(i, j).getPointList();
         let n = new THREE.Vector3(0, 1, 0);
 
-        this.mesh.get(i, j).getPointList().forEach(function (t) {
+        let pointList = this.mesh.get(i, j).getPointList();
+
+        pointList.forEach(function (t) {
           geo.vertices.push(new THREE.Vector3(t.x, f(t.x, t.y), t.y));
-          console.log(i, j, "::", t.x, t.y);
         });
 
         geo.faces.push(new THREE.Face3(k+0, k+1, k+2, n));
