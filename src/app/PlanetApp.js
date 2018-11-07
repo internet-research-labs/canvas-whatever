e701b141792c481591239df70821d7f5aa32c9ab
whatever <matt@svv.im>
2018-11-07 14:09:26 -0500

    :cherries:

--- src/app/PlanetApp.js
+++ src/app/PlanetApp.js
@@ -85,29 +85,12 @@ export default class PlanetApp {
     // this.renderer.setClearColor(0xFFFFFF);
     document.body.appendChild(this.renderer.domElement);
 
-    /*
-    let renderPass = new THREE.RenderPass(this.scene, this.camera);
-
-    let copyPass = new THREE.ShaderPass(THREE.CopyShader);
-    copyPass.renderToScreen = true;
-
-    this.rgbPass = new THREE.ShaderPass( THREE.RGBShiftShader );
-    this.rgbPass.uniforms[ 'amount' ].value = 0.0015;
-    this.rgbPass.renderToScreen = true;
-
-    this.composer = new THREE.EffectComposer(this.renderer);
-    this.composer.setSize(this.width, this.height);
-    this.composer.addPass(renderPass);
-    // this.composer.addPass(copyPass);
-    this.composer.addPass(this.rgbPass);
-    */
-
     let noise = new RepeatSimplexNoise2();
 
     // Meshes
     this.grids = [
-      grid((t, f) => { return 1*noise.at(2*t, 2*f)+6.0; }),
-      // grid((t, f) => { return 0.22*Math.sin(5*(t+f))+4.0; }, 10.0, 10.0),
+      // grid((t, f) => { return 0.11*noise.at(3*2*t, 3*2*f)+2.0; }),
+      grid((t, f) => { return 0.22*Math.sin(5*(t+f))+4.0; }, 10.0, 10.0),
       // grid((t, f) => { return random(0.0, 0.1)+0.22*Math.sin(5*(t+f))+4.0; }, 10.0, 10.0),
       // grid((t, f) => { return 4.0+random(0.0, 0.5); }),
       // grid((t, f) => { return 4.0; }),
@@ -116,9 +99,12 @@ export default class PlanetApp {
     // Grids
     this.group = new THREE.Group();
     this.grids.forEach((v, _) => {
-      v.rotation.x = Math.PI/2.0;
+      // v.rotation.x = Math.PI/2.0;
       this.group.add(v);
     });
+
+    this.groups = this.grids[0];
+    //this.groups.rotation.x = Math.PI/2.0;
 
     // Attach'em
     //let directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
@@ -128,16 +114,18 @@ export default class PlanetApp {
     let light1 = new THREE.DirectionalLight( 0xcccccc, 0.4 );
     let light2 = new THREE.PointLight( 0xff0000, 0.5 );
     let light3 = new THREE.PointLight( 0x00ffff, 0.5 );
-    light2.position.set(-2.8, 0.0, -15.0);
-    light3.position.set(-4.0, 0.0, -15.0);
+    light2.position.set(4.8, 10.0, -0.1);
+    light3.position.set(4.0, 10.0, -0.0);
 
     this.scene.add(light0);
-    this.scene.add(light1);
+    // this.scene.add(light1);
     this.scene.add(light2);
     this.scene.add(light3);
 
     this.scene.add(this.group);
     this.updatePosition(0.0, 0.0);
+    this.camera.position.set(13.0, 8.0, 0.0);
+    this.camera.lookAt(0.0, 4.0, 0.0);
     this.resize(this.width, this.height);
   }
 
@@ -161,7 +149,7 @@ export default class PlanetApp {
         // this.rgbPass.uniforms['amount'].value = Math.sqrt(u*u+v*v)/298.+0.002;
         let y = -1*(2*mouse.y/window.innerHeight - 1.0);
         let z = -1*(2*mouse.x/window.innerWidth - 1.0);
-        this.updatePosition(y, z);
+        //this.updatePosition(y, z);
       }),
     }
   }
@@ -177,7 +165,10 @@ export default class PlanetApp {
       this.meta.lastPosition.copy(this.camera.position);
       this.camera.lookAt(0.0, 0.0, 0.0);
     }
-    this.group.rotation.y = + new Date() / 1000.0;
+    // this.group.rotation.y = + new Date() / 1000.0;
+    let n = new THREE.Vector3(0.0, 1.0, 0.0);
+    n.normalize();
+    this.group.setRotationFromAxisAngle(n, new Date()/10000.);
   }
 
   setupCamera() {
